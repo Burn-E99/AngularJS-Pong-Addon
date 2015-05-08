bc440512fb6b6d70c9fcb0ced462190f6022a96e
Ean M <ean.milligan@gmail.com>
2015-05-08 10:29:02 -0400

    lolz

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ window.dp.controller('MainController', ['$scope', '$interval', '$state', functio
 }]);
 
 window.dp.controller('HomeController', ['$scope', function($scope) {
-	$scope.aboutmeContent = "Welcome to my Digital Portfolio(DP)!  My name is Ean Milligan.  I am currently in eleventh grade at The Dayton Regional STEM(Science Technology Engineering & Mathematics).  I took Geometry and Algebra 1 in eighth grade at St. Christopher.  I have taken Pre-Calc, Calc, and Calc BC.  I am very interested in technology and wish to enter the engineering field for college.";
-	$scope.watdpContent = "A DP is new way for STEM students to showcase their work.  This is required each year in order to move on to the next grade. Feel free to browse through my projects and experience, or contact me if you have questions or comments.";
+	$scope.aboutmeContent = "Welcome to the Pong Dev Page";
+	$scope.watdpContent = "Welcome to the Pong Dev Page";
 }]);
 
 window.dp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
