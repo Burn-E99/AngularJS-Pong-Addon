6ca0c60904409d5be8407ffa657c1f6efb236296
Ean M <ean.milligan@gmail.com>
2015-07-23 11:22:25 -0400

    continued work on layout

--- PongAddon/pong.js
+++ PongAddon/pong.js
@@ -1,7 +1,25 @@
 window.pong.controller('PongController', ['$scope', function($scope) {
+	var canvas = document.getElementById('pongField');
+	var ctx = canvas.getContext('2d');
+	
 	$scope.playPong = true;
+	$scope.leftCPU = false;
+	$scope.rightCPU = true;
 	
 	$scope.startPong = function(){
 		$scope.playPong = true;
 	};
+	
+	$scope.exitPong = function(){
+		$scope.playPong = false
+	}
+	
+	$scope.resizeCanvas = function(canvas){
+		canvas.style.width = '100%';
+		canvas.style.height = '100%';
+		canvas.width = canvas.offsetWidth;
+		canvas.height = canvas.offsetHeight;
+	};
+	
+	$scope.resizeCanvas(canvas);
 }]);
